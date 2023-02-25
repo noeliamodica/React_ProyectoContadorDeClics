d74ef1f6800142b1f47672144174424f80d495d3
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-02-25 14:04:22 -0300

    creacion componentes con su css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 
 import './App.css';
+import Logo from './imagenes/logo.png'
+import Boton from './componentes/Boton';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  }
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='logo-contenedor'>
+      <img className='imagen-logo'
+        src={Logo}
+        alt='Logo'
+      />
+      </div>
+
+      <div className='contenedor-principal'>
+        <Boton
+          texto= 'Clic'
+          esBotonDeCLic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto= 'Reiniciar'
+          esBotonDeCLic={false}
+          manejarClic={reiniciarContador}
+        />
+        </div>
+
     </div>
   );
 }
